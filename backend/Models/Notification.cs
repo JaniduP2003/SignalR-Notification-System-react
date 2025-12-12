3f84cf1397e9c6a8e8ba09261f7be45dec9f5062
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-12-12 09:51:42 +0530

    ADD::: conection STring in appsetings.js
    pusudo code for the data tables and the program.cs

---
+++ backend/Models/Notification.cs
@@ -0,0 +1,12 @@
+//part 1
+// add a table here to sore the data 
+//date created 
+//is it read or not lets make this a bool 
+// and the massge body
+
+//part2
+//add a eneum to suscess adn warning adn error 
+
+//part 3 
+//shuld a massage need a tittle or just a massage and id
+// add usir id and masssage then notofication 
