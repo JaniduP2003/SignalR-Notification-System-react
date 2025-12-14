ae412fa33e1ba386ff1f75c2f3515b5d1fb014ab
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-12-15 05:13:16 +0530

    ADD ::: the  MOdels

---
+++ backend/Hubs/NotificationHub.cs
@@ -0,0 +1,6 @@
+//Controllers/ → for API controllers
+//Hubs/ → for SignalR hubs
+// just like that contorller for the SignR
+
+//controlers are mostly REST req and resive
+//BUT hubs are REEEALTIME 
--- backend/Models/Notification.cs
+++ backend/Models/Notification.cs
@@ -1,8 +1,46 @@
+namespace NotificationApi.Models
+{
+    public class Notification
+    {
+        public string Id { get; set;} = Guid.NewGuid().ToString(); //Guid.NewGuid() generates a globally unique ID
+        public string Userid {get;set;} =string.Empty;
+        public string Title {get;set;} =string.Empty;
+        public string Massage {get;set;} = string.Empty;
+
+        public string IsReead {get;set;} = fulse;
+        public DeateTime CreatedAt {get;set;} =DateTime.UtcNow;
+
+        //add the eneum for the notification type call it here
+        //below make it and call it here 
+
+    }
+}
+
+public class SendNotificationRequest
+{
+    public string? USerId{get;set;} 
+    public string Title{get;set;} =string.Empty;
+    public string Message {get;set;}=string.Empty;
+    
+    //call the eneum here 
+}
+
+
+//lests mage the eneum
+
+public enum NotificationType{
+    info,
+    Success,
+    Warning,
+    error
+}
+
 //part 1
-// add a table here to sore the data 
+// add a table here to store the data 
 //date created 
 //is it read or not lets make this a bool 
 // and the massge body
+
 
 //part2
 //add a eneum to suscess adn warning adn error 
