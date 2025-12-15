5c7b8f7a24333e6e055277219349ac17ce49287a
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-12-15 05:34:22 +0530

    ADD::: the my override of on connect and disconnect
    OnConnectedAsync()
    
    its call when new signalR.HubConnectionBuilder()
      .withUrl("/notificationHub?userId=42")
      .build()
      .start();
    ➡️ As soon as .start() succeeds
    ➡️ SignalR calls OnConnectedAsync() on the server
    
    SO WE OVERIIDE WITH MY OWN ONE
    
    example:::
    public override async Task OnConnectedAsync()
    {
        Console.WriteLine("A client just connected");
        await base.OnConnectedAsync();
    }
    
    basicly----->   “When a client connects to this SignalR hub, run my async code.”

--- backend/Hubs/NotificationHub.cs
+++ backend/Hubs/NotificationHub.cs
@@ -3,4 +3,35 @@
 // just like that contorller for the SignR
 
 //controlers are mostly REST req and resive
-//BUT hubs are REEEALTIME 
+//BUT hubs are REEEALTIME
+//signR is FULLY ASYNC
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationApi.Hubs {
+
+    public class NotificationHub : Hub {
+        //:R this means it gets fetures of signalR
+        //all must be async 
+
+        public override async Task OnconnectedAsync(){
+        //gole is User 123 connected with connection ID: abcXYZ
+
+        }
+
+        public override async Task OnDisconnectedAsync(){
+            // do this my implementation when discinected 
+        }
+
+        public  async Task MarkAsRead(){
+
+        }
+
+        public async Task MarkAllAsRead(string userId){
+            
+        }
+
+    }
+
+    
+}
